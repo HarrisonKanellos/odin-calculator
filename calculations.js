c1e675c35ac60e533a9f3de8a3989215624f6012
HarrisonKanellos <harrisonkanellos@gmail.com>
2024-08-08 15:33:32 +1000

    Create function for each operation and variables to store input.
    Functions for add, subtract, multiply and divide.
    3 variables for first number, operator and second number.

---
+++ calculations.js
@@ -0,0 +1,19 @@
+let numInput1;
+let operatorInput;
+let numInput2;
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
