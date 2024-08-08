791754fd338425226c583a3a560def15855369c2
HarrisonKanellos <harrisonkanellos@gmail.com>
2024-08-08 15:45:19 +1000

    Create operate function
    Takes in 2 numbers and 1 operator and calls the relevant operation
    function based on the operator argument.

--- calculations.js
+++ calculations.js
@@ -17,3 +17,16 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, op, num2) {
+    switch (op) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1,num2);
+    }
+}
